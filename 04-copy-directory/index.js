b9e9ef30f4e529515ee4072c32f85a6c5e9c5e8a
ann-sm <annsmirnovva@gmail.com>
2025-01-19 20:43:35 +0300

    feat: complete task 04

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+  if (err) throw err;
+  console.log('Dir created');
+})
+
+fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+  if (err) throw err;
+  files.forEach(file => {
+    fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (err) => {
+      if (err) throw err;
+      console.log('Copied');
+    })
+  })
+})
+
