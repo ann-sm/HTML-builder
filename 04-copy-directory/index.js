ed96197d75b7f25e4a28570820165487698f3f49
ann-sm <annsmirnovva@gmail.com>
2025-01-19 21:57:19 +0300

    fix: add delete all files in copy folder feature

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,16 +1,31 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err, files) => {
   if (err) throw err;
+
+})
+
+fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
+  if (err) throw err;
+  if (files.length !== 0) {
+    files.forEach(file => {
+      fs.unlink(path.join(__dirname, 'files-copy', file), (err) => {
+        if (err) throw err;
+        console.log('Deleted');
+      });
+    })
+  }
 })
 
 fs.readdir(path.join(__dirname, 'files'), (err, files) => {
   if (err) throw err;
-  files.forEach(file => {
-    fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (err) => {
-      if (err) throw err;
+  if (files.length !== 0) {
+    files.forEach(file => {
+      fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (err) => {
+        if (err) throw err;
+      })
     })
-  })
+  }
 })
 
